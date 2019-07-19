5fb49c7fcb5159168b0dd52ccdebbf174d0866f7
morx3x <moro@studio.design>
2019-07-19 13:37:41 +0900

    return only channel and strip prefix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ function asyncParseString(xml: any) {
     return new Promise(function (resolve, reject) {
         const options = {
             trim: true,
-            explicitArray: false
+            explicitArray: false,
+            tagNameProcessors: [xml2js.processors.stripPrefix]
         };
 
         xml2js.parseString(xml, options, function (err: any, obj: any) {
@@ -17,8 +18,33 @@ function asyncParseString(xml: any) {
     });
 }
 
+// dotでアクセスできないkeyを置換する
+// アクセス可能なkey pattern: [a-zA-Z_$][0-9a-zA-Z_$]*
+// -> 冒頭が数値の場合は_を付加。マッチしない文字はすべて_に置換。
+// $は_に変換
+const getEscapedKey: any = (key: any) =>  {
+    return key.replace(/^([0-9])/, '_$1').replace(/[^0-9a-zA-Z_$]/g, '_').replace('$', '_');
+};
+
+const getEscapedData: any = (data: any) => {
+    if (!data || typeof data !== 'object') return data;
+    // array
+    if (Array.isArray(data)) {
+        return data.map(item => getEscapedData(item))
+    }
+    // object
+    return Object.entries(data).reduce(
+        (acc, [key, val]) => ({
+            ...acc,
+            [getEscapedKey(key)]: getEscapedData(val)
+        }),
+        {}
+    )
+};
+
 export async function toJson(url: string) {
     const res = await axios.get(url);
-    const obj =  await asyncParseString(res.data);
-    return JSON.stringify(obj);
+    const obj: any =  await asyncParseString(res.data);
+    const channel: any = getEscapedData(obj.rss.channel);
+    return JSON.stringify(channel);
 }
