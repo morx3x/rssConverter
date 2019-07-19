602e41e852c90c8573544e6025903d8e6a92c502
morx3x <moro@studio.design>
2019-07-19 12:15:35 +0900

    return only channel and strip prefix

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,6 +1,26 @@
 import {toJson} from '../src/index'
 
-test('Get rss json', async () => {
+test('Get rss json from note', async () => {
     const data: any = await toJson('https://note.mu/recommend/rss');
-    expect(data.hasOwnProperty('rss') !== -1).toBeTruthy();
+    expect(data.hasOwnProperty('item') !== -1).toBeTruthy();
+});
+
+test('Get rss json from bbc news', async () => {
+    const data: any = await toJson('http://feeds.bbci.co.uk/news/rss.xml#');
+    expect(data.hasOwnProperty('item') !== -1).toBeTruthy();
+});
+
+test('Get rss json from gigazine', async () => {
+    const data: any = await toJson('http://gigazine.net/news/rss_2.0/');
+    expect(data.hasOwnProperty('item') !== -1).toBeTruthy();
+});
+
+test('Get rss json from yahoo news', async () => {
+    const data: any = await toJson('https://news.yahoo.co.jp/pickup/rss.xml');
+    expect(data.hasOwnProperty('item') !== -1).toBeTruthy();
+});
+
+test('Get rss json from horiemon.com', async () => {
+    const data: any = await toJson('http://horiemon.com/feed/');
+    expect(data.hasOwnProperty('item') !== -1).toBeTruthy();
 });
